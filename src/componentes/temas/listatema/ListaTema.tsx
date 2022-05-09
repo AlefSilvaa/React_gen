9e54820279102f53fa24fa19daf8c2299fddcc1f
AlefSilvaa <asilvalivira@gmail.com>
2022-05-09 12:49:33 -0300

    Tema e Postagem

--- src/componentes/temas/listatema/ListaTema.tsx
+++ src/componentes/temas/listatema/ListaTema.tsx
@@ -1,45 +1,73 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {Box, Card, CardActions, CardContent, Button, Typography} from '@material-ui/core';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
 import './ListaTema.css';
+import Tema from '../../../models/Tema';
+import useLocalStorage from 'react-use-localstorage';
+import { busca } from '../../../service/Service';
 
 function ListaTema() {
-    return (
-        <>
-         <Box m={2}>
-             <Card variant="outlined">
-                 <CardContent>
-                  <Typography color="textSecondary" gutterBottom>
-                      Tema
-                  </Typography>
-                  <Typography variant='h5' component="h2">
-                      Minha descrição
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                    <Box display="flex" justifyContent="center"  mb={1.5} >
-                        
-                       <Link to="" className="text-descorator-none">
-                           <Box>
-                               <Button variant="contained" className='marginLeft' size='small' color='primary'>
-                                   atualizar
-                               </Button>
-                           </Box>
-                           </Link> 
-                           <Link to="" className='textt-decorator-none'>
-                               <Box>
-                                   <Button variant="contained" size='small' color="secondary">
-                                       deletar
-                                   </Button>
-                               </Box>
 
-                           </Link>
-                    </Box>
-                </CardActions>
-             </Card>
-         </Box>
-        </>
-    );
+  const [temas, setTemas] = useState<Tema[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let history = useNavigate();
+
+  useEffect(() => {
+    if(token == "") {
+      alert("Você precisa estar logago.")
+      history("/login")
+    }
+  }, [token])
+
+  async function getTema() {
+    await busca("/temas/all", setTemas, {
+      headers: {
+        'Authorization': token
+      }
+    })  
+  }
+
+  useEffect(() => {
+    getTema()
+  }, [temas.length])
+
+  return (
+    <>
+    {temas.map(tema =>(
+      <Box m={2} >
+        <Card variant="outlined" className='card-tema'>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Tema
+            </Typography>
+            <Typography variant="h5" component="h2" className='descricao-tema'>
+              {tema.descricao}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Box display="flex" justifyContent="center" mb={1.5} >
+
+              <Link to={`/formularioTema/${tema.id}`} className="text-decorator-none">
+                <Box mx={1}>
+                  <Button variant="contained" className="marginLeft, btn-atual-tema" size='small' color="primary" >
+                    atualizar
+                  </Button>
+                </Box>
+              </Link>
+              <Link to={`/deletarTema/${tema.id}`} className="text-decorator-none">
+                <Box mx={1}>
+                  <Button variant="contained" size='small' color="secondary" className='btn-del-tema'>
+                    deletar
+                  </Button>
+                </Box>
+              </Link>
+            </Box>
+          </CardActions>
+        </Card>
+      </Box>
+    ))}
+    </>
+  );
 }
 
 
